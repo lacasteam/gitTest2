bcff364d7299555d2968fa552639a65b7ad47250
Christian Lacaste <christianlacaste28@gmail.com>
2022-10-28 23:44:54 +0800

    get and display data from localStorage

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
+// const input_item = document.querySelector("#add_item");
+// const input_price = document.getElementById("add_price");
+
+// let items = {
+// 	cart: []
+// };
+
+// // console.log(`Item: ${input_item} Price: ${input_price}`);
+
+
+// const add_item = () => { 
+// 	let itemValue = input_item.value;
+// 	let priceValue = Number(input_price.value);
+
+// 	console.log(`Item: ${itemValue} Price: ${priceValue}`);
+// 	items.cart.push({item: itemValue, price: priceValue});
+// 	console.log(`The items ${JSON.stringify(items.cart)}`);
+
+// 	showcart();
+// 	localStorage.setItem('cart', JSON.stringify(items));
+// }
+
+// const showcart = () => { 
+// 	let x = document.getElementById("items_in_cart");
+// 	let current = "";
+// 	if(items.cart == ""){
+// 		x.innerHTML = "<b>No Items in Cart</b>";
+// 	}else{
+// 		items.cart.map((show_item)=>{
+// 			current += "<b>Item:</b>"+show_item.item + " <b>Price:</b> "+ show_item.price + "<hr>";
+// 		});
+// 		x.innerHTML = current;
+// 	}
+// 	clearInputs();
+// }
+
+// const clearInputs = () => { 
+// 	input_item.value = "";
+// 	input_price.value = "";
+
+// }
+
+// showcart();
+
+
+///////////////////////////////////////////////////////////// V2
+
 const input_item = document.querySelector("#add_item");
 const input_price = document.getElementById("add_price");
 
-let items = {
-	cart: []
-};
+let items;
 
-// console.log(`Item: ${input_item} Price: ${input_price}`);
+items = JSON.parse(localStorage.getItem("items"));
+console.log(`this is items ${items}`);
+
+if ((items = JSON.parse(localStorage.getItem("items"))) === null) {
+	items = {
+		cart: [],
+	};
+} else {
+	items = JSON.parse(localStorage.getItem("items"));
+}
+
 
 
 const add_item = () => { 
@@ -17,7 +72,7 @@ const add_item = () => {
 	console.log(`The items ${JSON.stringify(items.cart)}`);
 
 	showcart();
-	localStorage.setItem('cart', JSON.stringify(items));
+	localStorage.setItem('items', JSON.stringify(items));
 }
 
 const showcart = () => { 
@@ -41,4 +96,3 @@ const clearInputs = () => {
 }
 
 showcart();
-
